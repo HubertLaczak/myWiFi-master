1333405d97b3adff31507d6f3dcd57002b345b04
Dell <hlaczak@gmail.com>
2019-01-20 17:41:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dell/mywifi/MainActivity.java
@@ -0,0 +1,336 @@
+package com.example.dell.mywifi;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.NetworkOnMainThreadException;
+import android.os.StrictMode;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button send, btServer, btClient;
+    EditText writeMsg;
+    TextView status, IPadd, myMessa;
+
+    SendReceive sendReceive;
+
+    static final int STATE_LISTENING = 1;
+    static final int STATE_CONNECTING = 2;
+    static final int STATE_CONNECTED = 3;
+    static final int STATE_CONNECTION_FAILED = 4;
+    static final int STATE_MESSAGE_RECEIVED = 5;
+
+    LinearLayout upper, lower;
+    String txtNickName;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }       //https://stackoverflow.com/questions/43511365/how-to-socket-thread-in-android-api-25
+        findViewByID();
+        implementListeners();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        txtNickName = sharedPref.getString("NickName", "User");
+        Toast.makeText(this, "Witaj " + txtNickName + "!", Toast.LENGTH_SHORT).show();
+    }
+
+    private class ServerClass extends Thread{
+        private ServerSocket serverSocket;
+        Socket socket;
+        static final int socketServerPORT = 8080;
+
+        public ServerClass(){
+        }
+
+        @Override
+        public void run()
+        {
+            try {
+                serverSocket = new ServerSocket(socketServerPORT);
+                while (socket == null) {
+                    socket = serverSocket.accept();
+                    Message message = Message.obtain();
+                    message.what = STATE_CONNECTING;
+                    handler.sendMessage(message);
+                }
+                if (socket != null) {
+                    Message message = Message.obtain();
+                    message.what = STATE_CONNECTED;
+                    handler.sendMessage(message);
+                    sendReceive = new SendReceive(socket);
+                    sendReceive.start();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public String getIpAddress() {
+            String ip = "";
+            try {
+                Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                        .getNetworkInterfaces();
+                while (enumNetworkInterfaces.hasMoreElements()) {
+                    NetworkInterface networkInterface = enumNetworkInterfaces
+                            .nextElement();
+                    Enumeration<InetAddress> enumInetAddress = networkInterface
+                            .getInetAddresses();
+                    while (enumInetAddress.hasMoreElements()) {
+                        InetAddress inetAddress = enumInetAddress
+                                .nextElement();
+
+                        if (inetAddress.isSiteLocalAddress()) {
+                            ip += "Server running at : "
+                                    + inetAddress.getHostAddress();
+                        }
+                    }
+                }
+
+            } catch (SocketException e) {
+                e.printStackTrace();
+                ip += "Something Wrong! " + e.toString() + "\n";
+            }
+            return ip;
+        }
+        public int getPort() {
+            return socketServerPORT;
+        }
+    }
+
+    private class ClientClass extends Thread{
+        String dstAddress;
+        int dstPort;
+
+        public ClientClass() {
+            dstAddress = "192.168.1.23";//moje
+            dstPort = 8080;//moje
+        }
+
+        public void run(){
+            Socket socket;
+            try {
+                socket = new Socket(dstAddress,dstPort);
+                Message message = Message.obtain();
+                message.what = STATE_CONNECTED;
+                handler.sendMessage(message);
+                sendReceive = new SendReceive(socket);
+                sendReceive.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Message message = Message.obtain();
+                message.what = STATE_CONNECTION_FAILED;
+                handler.sendMessage(message);
+            }
+//        finally {
+//                if (socket != null) {
+//                    try {
+//                        socket.close();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+        }
+    }
+
+    private class SendReceive extends Thread{
+        private final Socket socket2;
+        private final InputStream inputStream;
+        private final OutputStream outputStream;
+        Message msg;
+
+        private SendReceive(Socket socket){
+            this.socket2=socket;
+            InputStream tempIn = null;
+            OutputStream tempOut = null;
+
+            try {
+                tempIn = socket2.getInputStream();
+                tempOut = socket2.getOutputStream();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            inputStream = tempIn;
+            outputStream = tempOut;
+
+        }
+        public void run(){
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            while(true){
+                try {
+                    bytes = inputStream.read(buffer);
+                    handler.obtainMessage(STATE_MESSAGE_RECEIVED, bytes, -1, buffer).sendToTarget();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void write(byte[] bytes){
+            try {
+                outputStream.write(bytes);
+                byte[] readBuff = (byte[]) msg.obj;
+                String tempMsg = new String(readBuff, 0 ,msg.arg1);
+                myMessa.setText(tempMsg);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            catch (NullPointerException e1) {
+                e1.printStackTrace();
+            }
+
+        }
+    }
+
+
+
+    Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what){
+                case STATE_LISTENING:
+                    status.setText("Listening");
+                    break;
+                case STATE_CONNECTING:
+                    status.setText("Connecting  ");
+                    break;
+                case STATE_CONNECTED:
+                    status.setText("Connected");
+                    btClient.setEnabled(false);
+                    btServer.setEnabled(false);
+//                    IPadd.setVisibility(View.GONE);
+                    break;
+                case STATE_CONNECTION_FAILED:
+                    status.setText("Connecting Failed");
+                    break;
+                case STATE_MESSAGE_RECEIVED:
+                    byte[] readBuff = (byte[]) msg.obj;
+                    String tempMsg = new String(readBuff,0,msg.arg1);
+                    myMessa.setText(tempMsg);
+                    break;
+            }
+            return true;
+        }
+    });
+
+    private void implementListeners() {
+        btClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClientClass clientClass = new ClientClass();
+                clientClass.start();
+                btServer.setEnabled(false);
+            }
+        });
+
+        btServer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ServerClass serverClass = new ServerClass();
+                serverClass.start();
+                IPadd.setText(serverClass.getIpAddress() +" and port: " + serverClass.getPort());
+                btClient.setEnabled(false);
+
+
+            }
+        });
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String string = String.valueOf(txtNickName + ": " + writeMsg.getText());
+                sendReceive.write(string.getBytes());
+            }
+        });
+
+
+    }
+
+    public void findViewByID(){
+        status = findViewById(R.id.tvStatus);
+        btServer = findViewById(R.id.btServer);
+        btClient = findViewById(R.id.btClient);
+        IPadd = findViewById(R.id.TojestmojeID);
+        send = findViewById(R.id.btnSend);
+        writeMsg = findViewById(R.id.etMessage);
+        myMessa = findViewById(R.id.myMessa);
+
+        upper = findViewById(R.id.upperLayout);
+        lower = findViewById(R.id.lowerLayout);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item2:
+                openActivityMessage();
+                break;
+            case  R.id.item1:
+                Toast.makeText(this, "This window already opened", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void openActivityMessage() {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//            intent.putExtra("ip", dstAddress);
+//            intent.putExtra("port", ip);
+        startActivity(intent);
+        finish();
+    }
+}
+
---
+++ app/src/main/java/com/example/dell/mywifi/SecondActivity.java
@@ -0,0 +1,193 @@
+package com.example.dell.mywifi;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.net.wifi.WifiManager;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Locale;
+
+public class SecondActivity extends AppCompatActivity {
+Button btn_language, btn_OnOff, btn_discover, btn_changeNick;
+EditText text_newNick;
+String language;
+String txtNickName;
+TextView WelcomeMessage;
+
+SharedPreferences sharedPref;
+
+WifiManager wifiManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        findViewByID();
+
+        clickMe();
+    }
+
+
+
+    private void clickMe() {
+        btn_OnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (wifiManager.isWifiEnabled()){
+                    wifiManager.setWifiEnabled(false);
+                    btn_OnOff.setText(R.string.btn_On);
+                } else {
+                    wifiManager.setWifiEnabled(true);
+                    btn_OnOff.setText(R.string.btn_Off);
+                }
+            }
+        });
+
+
+
+        btn_language.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showChangeLanguageDialog();
+            }
+        });
+
+        btn_changeNick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeNickName(text_newNick.getText().toString());
+                text_newNick.setText("");
+            }
+        });
+
+        btn_discover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void changeNickName(String newNick) {
+//        sharedPref = getPreferences(Context.MODE_PRIVATE);
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("NickName", newNick);
+        editor.commit();
+        Toast.makeText(this, R.string.ToastChangedNick, Toast.LENGTH_SHORT).show();
+        WelcomeMessage.setText(getString(R.string.textWelcome) + " " + newNick + "!");
+
+        int test = 1;
+
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        readNickAndSetTextV();
+    }
+
+    private void readNickAndSetTextV(){
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        txtNickName = sharedPref.getString("NickName", "User");
+        WelcomeMessage.setText(getString(R.string.textWelcome) + " " + txtNickName + "!");
+    }
+
+    private void findViewByID() {
+        btn_language = findViewById(R.id.btn_language);
+        btn_OnOff = findViewById(R.id.btn_OnOff);
+        btn_discover = findViewById(R.id.btn_Discover);
+        btn_changeNick = findViewById(R.id.btn_changeNick);
+        WelcomeMessage = findViewById(R.id.WelcomeMessage);
+        WelcomeMessage.setText(txtNickName);
+
+        text_newNick = findViewById(R.id.text_newNick);
+
+        if (wifiManager.isWifiEnabled()){
+            btn_OnOff.setText(R.string.btn_Off);
+        } else {
+            btn_OnOff.setText(R.string.btn_On);
+        }
+    }
+
+
+    private void showChangeLanguageDialog() {
+        final String[] listItems = {"English", "Polski"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(SecondActivity.this) //nowe okno AlertDialog
+                .setTitle(R.string.AlertDialog_Builder_title); //tytuł okienka z wyborem języków //Available Languages
+        builder.setSingleChoiceItems(listItems, -1, new DialogInterface.OnClickListener() { //-1 oznacza, że żadna z opcji nie jest "checked", nie jest zaznaczona
+            @Override //Dialog Interface is..Interface used to allow the creator of a dialog to run some code when an item on the dialog is clicked.
+            public void onClick(DialogInterface dialog, int which) {
+                if(which == 0){
+                    language = "en";
+                    setLocale("en");
+                    recreate();// wywołuje onDestroy() oraz onCreate() na tej Activity. Tworzy się nowa instancja Activity
+                    readNickAndSetTextV();
+                } else {
+                    language = "pl";
+                    setLocale("pl");
+                    recreate();
+                    readNickAndSetTextV();
+                }
+            }
+        })
+                .setIcon(R.mipmap.language) //ustawienie ikonki języków
+                .show();    //wyświetla AlertDialog
+    }
+//
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang); //obiekt reprezentujący ustawienia geograficzne, polityczne itp.
+        Locale.setDefault(locale);  //ustawienia lokalne dla JVM względem obiektu locale
+        Configuration config = new Configuration();
+        config.setLocale(locale); // //zmiana ustawień regionalnych
+        getResources().updateConfiguration(config, getResources().getDisplayMetrics()); //uaktualnianie danych :)
+        //getResources()..Umożliwia dostęp do zasobów i klas związanych z aplikacją, a także do wywołań dla
+        // operacji na poziomie aplikacji, takich jak uruchamianie działań, nadawanie i otrzymywanie zamiarów itp.
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                openActivityMessage();
+                break;
+            case  R.id.item2:
+                Toast.makeText(this, "This window already opened", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void openActivityMessage() {
+        Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//            intent.putExtra("ip", dstAddress);
+//            intent.putExtra("port", ip);
+        startActivity(intent);
+        finish();
+    }
+}
